0f78f70143cf4e0b4b6d931a88056a3a4aacd6df
harshad-wohlig <harshad.shirsat@wohlig.com>
2017-05-12 18:25:21 +0530

    Removed syntax errors

--- api/services/Know.js
+++ api/services/Know.js
@@ -30,88 +30,94 @@ module.exports = mongoose.model('Know', schema);
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {
     getAll: function (data, callback) {
-        this.find({}).exec(function (err, found) {
-                if (err) {
-                    console.log(err);
-                    callback(err, null);
-                } else if (found && found.length > 0) {
-                    callback(null, found);
-                });
-        }
-        else {
-            callback(null, []);
-        }
-    });
-},
-getOne: function (data, callback) {
-this.findOne({
-    "_id": data._id
-}).exec(function (err, found) {
-        if (err) {
-            console.log(err);
-            callback(err, null);
-        } else if (found && Object.keys(found).length > 0) {
-            callback(null, found);
-        });
-}
-else {
-    callback(null, {});
-}
-});
-},
-findLimited: function (data, callback) {
-    var newreturns = {};
-    newreturns.data = [];
-    var check = new RegExp(data.search, "i");
-    data.pagenumber = parseInt(data.pagenumber);
-    data.pagesize = parseInt(data.pagesize);
-    async.parallel([
-            function (callback) {
-                Know.count({
-                    name: {
-                        '$regex': check
-                    }
-                }).exec(function (err, number) {
-                    if (err) {
-                        console.log(err);
-                        callback(err, null);
-                    } else if (number && number !== "") {
-                        newreturns.total = number;
-                        newreturns.totalpages = Math.ceil(number / data.pagesize);
-                        callback(null, newreturns);
-                    } else {
-                        callback(null, newreturns);
-                    }
-                });
-            },
-            function (callback) {
-                Know.find({
-                    name: {
-                        '$regex': check
-                    }
-                }).skip(data.pagesize * (data.pagenumber - 1)).limit(data.pagesize).exec(function (err, data2) {
-                    if (err) {
-                        console.log(err);
-                        callback(err, null);
-                    } else if (data2 && data2.length > 0) {
-                        newreturns.data = data2;
-                        callback(null, newreturns);
-                    } else {
-                        callback(null, newreturns);
-                    }
-                });
-            }
-        ],
-        function (err, data4) {
+        Know.find({}).exec(function (err, found) {
             if (err) {
                 console.log(err);
                 callback(err, null);
-            } else if (data4) {
-                callback(null, newreturns);
+            } else if (found && found.length > 0) {
+                callback(null, found);
             } else {
-                callback(null, newreturns);
+                callback({
+                    message: {
+                        data: "Invalid Credentials!"
+                    }
+                }, null);
             }
         });
-}
+    },
+
+    getOne: function (data, callback) {
+        Know.findOne({
+            "_id": data._id
+        }).exec(function (err, found) {
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            } else if (found && Object.keys(found).length > 0) {
+                callback(null, found);
+            } else {
+                callback({
+                    message: {
+                        data: "Invalid Credentials!"
+                    }
+                }, null);
+            }
+        });
+    },
+
+    findLimited: function (data, callback) {
+        var newreturns = {};
+        newreturns.data = [];
+        var check = new RegExp(data.search, "i");
+        data.pagenumber = parseInt(data.pagenumber);
+        data.pagesize = parseInt(data.pagesize);
+        async.parallel([
+                function (callback) {
+                    Know.count({
+                        name: {
+                            '$regex': check
+                        }
+                    }).exec(function (err, number) {
+                        if (err) {
+                            console.log(err);
+                            callback(err, null);
+                        } else if (number && number !== "") {
+                            newreturns.total = number;
+                            newreturns.totalpages = Math.ceil(number / data.pagesize);
+                            callback(null, newreturns);
+                        } else {
+                            callback(null, newreturns);
+                        }
+                    });
+                },
+                function (callback) {
+                    Know.find({
+                        name: {
+                            '$regex': check
+                        }
+                    }).skip(data.pagesize * (data.pagenumber - 1)).limit(data.pagesize).exec(function (err, data2) {
+                        if (err) {
+                            console.log(err);
+                            callback(err, null);
+                        } else if (data2 && data2.length > 0) {
+                            newreturns.data = data2;
+                            callback(null, newreturns);
+                        } else {
+                            callback(null, newreturns);
+                        }
+                    });
+                }
+            ],
+            function (err, data4) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else if (data4) {
+                    callback(null, newreturns);
+                } else {
+                    callback(null, newreturns);
+                }
+            });
+    }
 };
 module.exports = _.assign(module.exports, exports, model);
