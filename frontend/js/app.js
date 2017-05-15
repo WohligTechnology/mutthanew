b5c0c348bb0bdcf84ef65db696fa5735c0c2c9f7
harshad-wohlig <harshad.shirsat@wohlig.com>
2017-05-15 11:33:47 +0530

    frontend ui pages added. app routing added

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -19,16 +19,70 @@ myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locat
     $httpProvider.defaults.withCredentials = true;
     $stateProvider
         .state('home', {
-            url: "/",
-            templateUrl: tempateURL,
+            url: "/home",
+            templateUrl: "views/template.html",
             controller: 'HomeCtrl'
         })
-        .state('form', {
-            url: "/form",
-            templateUrl: tempateURL,
-            controller: 'FormCtrl'
+        //  .state('footer', {
+        //   url: "/",
+        //   templateUrl: "views/template.html",
+        //   controller: 'FooterCtrl'
+        // })
+        .state('know', {
+            url: "/know",
+            templateUrl: "views/template.html",
+            controller: 'KnowCtrl'
+        })
+        .state('careers', {
+            url: "/careers",
+            templateUrl: "views/template.html",
+            controller: 'CareersCtrl'
+        })
+        .state('client', {
+            url: "/client",
+            templateUrl: "views/template.html",
+            controller: 'ClientCtrl'
+        })
+        .state('contact', {
+            url: "/contact",
+            templateUrl: "views/template.html",
+            controller: 'ContactCtrl'
+        })
+        // .state('media', {
+        //   url: "/media",
+        //   templateUrl: "views/template.html",
+        //   controller: 'MediaCtrl'
+        // })
+        .state('project', {
+            url: "/project/:id",
+            templateUrl: "views/template.html",
+            controller: 'ProjectCtrl'
+        })
+        // .state('projects', {
+        //     url: "/projects/6",
+        //     templateUrl: "views/template.html",
+        //     controller: 'ProjectsCtrl'
+        //   })
+        .state('build', {
+            url: "/build",
+            templateUrl: "views/template.html",
+            controller: 'BuildCtrl'
+        })
+        .state('give', {
+            url: "/give",
+            templateUrl: "views/template.html",
+            controller: 'GiveCtrl'
+        }).state('shantilal', {
+            url: "/give/shantilal",
+            templateUrl: "views/template.html",
+            controller: 'ShantilalCtrl'
+        })
+        .state('bjs', {
+            url: "/give/bjs",
+            templateUrl: "views/template.html",
+            controller: 'BjsCtrl'
         });
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/home");
     $locationProvider.html5Mode(isproduction);
 });
 
